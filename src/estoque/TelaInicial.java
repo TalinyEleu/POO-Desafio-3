94f39b05ca4453637df6bbb1d883dc338c3935e8
jesse_jesus <jesse_jesus@estudante.sesisenai.org.br>
2025-11-24 20:04:15 -0300

    Commiting Via Git For windows First Try.

---
+++ src/estoque/TelaInicial.java
@@ -0,0 +1,95 @@
+package estoque;
+
+
+//CURSO: GRADS | STDC
+//UC: GR POO - 2025/2 - 35761 - Programação Orientada a Objetos
+//DESAFIO: 3 Gerenciamento de Estoque
+//GRUPO: Nº 16
+//INTEGRANTES:(Nataliny Eleutério da Silva / Jessé Oliveira de Jesus)
+//Arquivo: classe java pra exibição da tela inicial do app
+
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class TelaInicial  extends JFrame {
+
+		private JPanel panel;
+		
+		public TelaInicial() {
+			setTitle("Gestão de estoque");
+			setLayout(new FlowLayout());
+			
+			this.panel = new JPanel();
+			this.panel .setLayout(new FlowLayout());
+			this.panel .setPreferredSize(new Dimension(500, 800));
+			add(this.panel);
+			
+			//criando botoes na interface
+			criarBotao("Cadastrar", new ButtonCadastrarHandler());
+			criarBotao("Listar", new ButtonListarHandler());
+			criarBotao("Excluir", new ButtonExcluirrHandler());
+			criarBotao("Exportar", new ButtonExportarHandler());
+			criarBotao("Sair", new ButtonSairHandler());
+			
+			
+			setSize(new Dimension(500, 800));
+			setPreferredSize(new Dimension(500, 800));
+			setVisible(true);
+			setDefaultCloseOperation(EXIT_ON_CLOSE);
+			
+	}
+		private void criarBotao(String label, ActionListener handler) {
+			JButton button = new JButton(label);
+			button.addActionListener(handler);
+			button.setPreferredSize(new Dimension(300, 80));
+			this.panel.add(button);
+		}
+		
+		private class ButtonCadastrarHandler implements ActionListener{
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		}
+		
+		
+		private class ButtonListarHandler implements ActionListener{
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				new Listagem();
+			}
+		}
+		
+		private class ButtonExcluirrHandler implements ActionListener{
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		}
+		
+		private class ButtonExportarHandler implements ActionListener{
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		}
+		
+		private class ButtonSairHandler implements ActionListener{
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		}	
+}
+	
+
